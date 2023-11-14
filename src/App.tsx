fee7f7f68af9b5f693de8f10e8f198d68dfa7bd2
thezoot3 <thezoot3@gmail.com>
2023-11-15 00:52:45 +0900

    ASDFG

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    const element = document.documentElement; // 전체 화면으로 보여줄 요소 선택
-    if (element.requestFullscreen) {
-        element.requestFullscreen().then(r => console.log); // 전체 화면 진입 요청
+    function fulls() {
+        const element = document.documentElement; // 전체 화면으로 보여줄 요소 선택
+        if (element.requestFullscreen) {
+            element.requestFullscreen().then(r => console.log); // 전체 화면 진입 요청
+        }
     }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" onClick={fulls}/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
