3af08533b699c446095f010ce098207121c0d3af
Ryu Siwoo <61928790+thezoot3@users.noreply.github.com>
2023-11-15 08:39:36 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    function fulls() {
-        const element = document.documentElement; // 전체 화면으로 보여줄 요소 선택
-        if (element.requestFullscreen) {
-            element.requestFullscreen().then(r => console.log); // 전체 화면 진입 요청
-        }
-    }
+    // 플래시 켜기
+function turnOnFlashlight() {
+  navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
+    .then(function(stream) {
+      const track = stream.getVideoTracks()[0];
+      const imageCapture = new ImageCapture(track);
+      const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
+        track.applyConstraints({
+          advanced: [{ torch: true }]
+        });
+      });
+    })
+    .catch(function(err) {
+      console.log('Error: ', err);
+    });
+}
 
+// 플래시 끄기
+function turnOffFlashlight() {
+  navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
+    .then(function(stream) {
+      const track = stream.getVideoTracks()[0];
+      track.applyConstraints({
+        advanced: [{ torch: false }]
+      });
+    })
+    .catch(function(err) {
+      console.log('Error: ', err);
+    });
+}
   return (
     <div className="App">
+        <button onClick={turnOnFlashlight}>키기</button>
+        <button onClick={turnOffFlashlight}>끄기</button>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" onClick={fulls}/>
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
